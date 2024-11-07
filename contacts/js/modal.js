1a9e7f4bddad223f15dc86adcf0dc0c26442b54a
Христофоров Иван(тк) <IvHristoforov@alabuga.local>
2024-11-07 15:16:39 +0300

    update contacts page(tk)

--- contacts/js/modal.js
+++ contacts/js/modal.js
@@ -1,25 +1,42 @@
-document.getElementById('openModal').addEventListener('click', function() {
-    const modal = document.getElementById('modal');
+// Получаем элементы модального окна и кнопки открытия
+const modal = document.getElementById('modal');
+const openModalButton = document.getElementById('openModal');
+
+// Добавляем обработчик события для открытия модального окна
+openModalButton.addEventListener('click', function() {
     modal.classList.add('show');
 });
 
-document.getElementsByClassName('close')[0].addEventListener('click', function() {
-    const modal = document.getElementById('modal');
+// Получаем кнопку закрытия модального окна
+const closeModalButton = document.getElementsByClassName('close')[0];
+
+// Добавляем обработчик события для закрытия модального окна по клику на кнопку
+closeModalButton.addEventListener('click', function() {
     modal.classList.remove('show');
 });
 
+// Добавляем обработчик события для закрытия модального окна по клику вне его
 window.addEventListener('click', function(event) {
-    if (event.target == document.getElementById('modal')) {
-        document.getElementById('modal').classList.remove('show');
+    if (event.target == modal) {
+        modal.classList.remove('show');
     }
 });
 
+// Добавляем обработчик события для закрытия модального окна по нажатию клавиши "Escape"
+window.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape' && modal.classList.contains('show')) {
+        modal.classList.remove('show');
+    }
+});
+
+// Функция для отправки формы
 function submitForm() {
     const form = document.getElementById('contactForm');
     const formData = {};
 
     let isValid = true;
 
+    // Собираем данные из формы
     for (let i = 0; i < form.elements.length; i++) {
         const element = form.elements[i];
         if (element.name && element.value) {
@@ -41,13 +58,15 @@ function submitForm() {
         isValid = false;
     }
 
+    // Если форма валидна, выводим данные в консоль, закрываем модальное окно и очищаем поля формы
     if (isValid) {
         console.log(formData);
-        document.getElementById('modal').classList.remove('show');
+        modal.classList.remove('show'); // Закрываем модальное окно после успешной валидации
         clearFormFields();
     }
 }
 
+// Функция для очистки полей формы
 function clearFormFields() {
     const form = document.getElementById('contactForm');
     for (let i = 0; i < form.elements.length; i++) {
