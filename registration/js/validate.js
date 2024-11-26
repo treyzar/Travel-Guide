9434daee13df8d39c4b96b4b050dd77188c83c71
Ivan Hristoforov <khristoforov2008@gmail.com>
2024-11-26 12:11:26 +0300

    new changes

--- registration/js/validate.js
+++
@@ -1,8 +0,0 @@
-const username = document.getElementById('username')
-const password = document.getElementById('password')
-if (username === ''){
-    alert("Поле вводе не должно быть пустым")
-}
-if (password === '') {
-    alert('Поле ввода не должно бытьт пустым')
-}
