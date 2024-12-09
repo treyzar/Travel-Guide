cc073afcd98bc15d8c3e5c0639cc2a82f5f7a529
Ivan Hristoforov <khristoforov2008@gmail.com>
2024-12-09 09:48:11 +0300

    изменен путь к css файлу, подправлены стили для корректного отображения элементов

--- main/js/main.js
+++ main/js/main.js
@@ -1,31 +1,31 @@
-document.addEventListener('DOMContentLoaded', function() {
-  let isSignedIn = sessionStorage.getItem('sign');
+document.addEventListener("DOMContentLoaded", function () {
+  let isSignedIn = sessionStorage.getItem("sign");
 
-  if (isSignedIn === 'true') {
-    console.log('Пользователь авторизован');
-    document.getElementById('sign').style.display = 'none';
-    document.getElementById('reg').style.display = 'none';
-    document.getElementById('logout').style.display = 'block';
+  if (isSignedIn === "true") {
+    console.log("Пользователь авторизован");
+    document.getElementById("sign").style.display = "none";
+    document.getElementById("reg").style.display = "none";
+    document.getElementById("logout").style.display = "block";
 
-    document.getElementById('logout').addEventListener('click', function() {
+    document.getElementById("logout").addEventListener("click", function () {
       isSignedIn = false;
-      document.getElementById('sign').style.display = 'block';
-      document.getElementById('reg').style.display = 'block';
-      document.getElementById('logout').style.display = 'none';
-      sessionStorage.removeItem('sign');
+      document.getElementById("sign").style.display = "block";
+      document.getElementById("reg").style.display = "block";
+      document.getElementById("logout").style.display = "none";
+      sessionStorage.removeItem("sign");
     });
   }
 });
 
-const slider = document.querySelector('.slider');
-const prevButton = document.querySelector('.prev-button');
-const nextButton = document.querySelector('.next-button');
-const slides = Array.from(slider.querySelectorAll('img'));
+const slider = document.querySelector(".slider");
+const prevButton = document.querySelector(".prev-button");
+const nextButton = document.querySelector(".next-button");
+const slides = Array.from(slider.querySelectorAll("img"));
 const slideCount = slides.length;
 let slideIndex = 0;
 
-prevButton.addEventListener('click', showPreviousSlide);
-nextButton.addEventListener('click', showNextSlide);
+prevButton.addEventListener("click", showPreviousSlide);
+nextButton.addEventListener("click", showNextSlide);
 
 function showPreviousSlide() {
   slideIndex = (slideIndex - 1 + slideCount) % slideCount;
@@ -40,9 +40,9 @@ function showNextSlide() {
 function updateSlider() {
   slides.forEach((slide, index) => {
     if (index === slideIndex) {
-      slide.style.display = 'block';
+      slide.style.display = "block";
     } else {
-      slide.style.display = 'none';
+      slide.style.display = "none";
     }
   });
 }
